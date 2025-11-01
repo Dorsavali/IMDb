a41d35c2474d0c6f18cf481e43de9031e449786f
Dorsa Valli <dorsavali@yahoo.com>
2025-11-01 22:07:53 +0330

    mobile header done

--- src/js/Header/Header.js
+++ src/js/Header/Header.js
@@ -1,4 +1,5 @@
 async function logos() {
+  /*logo svg*/
   let logoRes = await fetch("http://localhost:3000/logo");
   let logoData = await logoRes.json();
 
@@ -14,7 +15,7 @@ async function logos() {
     .join("");
 
   document.querySelector(".logo").insertAdjacentHTML("afterbegin", logoHTML);
-
+  /*menu svg*/
   let menuRes = await fetch("http://localhost:3000/menuLogo");
   let menuData = await menuRes.json();
 
@@ -32,25 +33,89 @@ async function logos() {
   document
     .querySelector(".menuLogo")
     .insertAdjacentHTML("afterbegin", menuHTML);
+  /*search bar */
+  let searchRes = await fetch("http://localhost:3000/searchLogo");
+  let searchData = await searchRes.json();
 
-    let searchRes = await fetch("http://localhost:3000/searchLogo");
-    let searchData = await searchRes.json();
-  
-    let searchHTML = searchData
-      .map((item) => {
-        return `
+  let searchHTML = searchData
+    .map((item) => {
+      return `
         
           <input type="search" name="search" id="search" placeholder="">
           <img src="${item.src}" alt="${item.alt}">
         
       `;
+    })
+    .join("");
+
+  document
+    .querySelector(".search")
+    .insertAdjacentHTML("afterbegin", searchHTML);
+  /*imdb pro */
+  let proRes = await fetch("http://localhost:3000/proLogo");
+  let proData = await proRes.json();
+
+  let proHTML = proData
+    .map((item) => {
+      return `
+          <img src="${item.src}" alt="${item.alt}">
+      `;
+    })
+    .join("");
+
+  document.querySelector(".pro").insertAdjacentHTML("afterbegin", proHTML);
+  /*watchlist */
+  let watchlistRes = await fetch("http://localhost:3000/watchlistLogo");
+  let watchlistData = await watchlistRes.json();
+
+  let watchlistHTML = watchlistData
+    .map((item) => {
+      return `
+          <img src="${item.src}" alt="${item.alt}">${item.title}
+      `;
+    })
+    .join("");
+
+  document.querySelector(".watchlist").insertAdjacentHTML("afterbegin", watchlistHTML);
+    /*signin */
+    let signInRes = await fetch("http://localhost:3000/signIn");
+    let signInData = await signInRes.json();
+  
+    let signInHTML = signInData
+      .map((item) => {
+        return `
+           ${item.title}
+        `;
       })
       .join("");
   
-    document
-      .querySelector(".search")
-      .insertAdjacentHTML("afterbegin", searchHTML);
+    document.querySelector(".signIn").insertAdjacentHTML("afterbegin", signInHTML);
+        /*useApp */
+        let useAppRes = await fetch("http://localhost:3000/useApp");
+        let useAppData = await useAppRes.json();
+      
+        let useAppHTML = useAppData
+          .map((item) => {
+            return `
+               <a href = "${item.href}">${item.title}</a>
+            `;
+          })
+          .join("");
+      
+        document.querySelector(".useApp").insertAdjacentHTML("afterbegin", useAppHTML);
+          /*lang */
+  let langRes = await fetch("http://localhost:3000/lang");
+  let langData = await langRes.json();
 
+  let langHTML = langData
+    .map((item) => {
+      return `
+          <img src="${item.src}" alt="${item.alt}">${item.title}
+      `;
+    })
+    .join("");
+
+  document.querySelector(".lang").insertAdjacentHTML("afterbegin", langHTML);
 }
 
 export default logos;
